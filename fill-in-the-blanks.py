325eae03d86eb5ef3e496df89fbc0dd7a80b6f62
leenpaws <s.chhaya@gmail.com>
2016-06-03 15:32:17 -0400

    Update

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -15,21 +15,6 @@
 
 # To help you get started, we've provided a sample paragraph that you can use when testing your code.
 # Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-
-def gamebegin():
-    level = raw_input("Please provide answer in all lower case!!" \
-                      "Please choose a level 1. easy 2. medium 3. hard : ")
-
-    if level == '3':
-        return play_game(test_string3, parts_of_speech, answer3)
-    if level == '2':
-        return play_game(test_string2, parts_of_speech, answer2)
-    if level == '1':
-        return play_game(test_string1, parts_of_speech, answer1)
-    return False
-
-
 # A list of replacement words to be passed in to the play game function.
 parts_of_speech = ["__1__", "__2__", "__3__", "__4__"]
 answer1 = ["attributes", "opening", "closing", "markup"]
@@ -47,7 +32,21 @@ test_string2 = "Ways of Thinking: Blank1 thinking involves creating perfectly cl
                "a big problem into smaller pieces.  __4__ is a systematic process of relentlessly identifying the cause of a computer program" \
                "failure"
 test_string3 = "Rules Summary for Normalized Tables: 1. Every row has the same number of __1__.  2.  There is a  unique key and everything " \
-               "in the row says something about the __2__.  "
+
+
+def gamebegin():
+    level = raw_input("Please provide answer in all lower case!!" \
+                      "Please choose a level 1. easy 2. medium 3. hard : ")
+    if level == '3':
+        print test_string3
+        return play_game(test_string3, parts_of_speech, answer3)
+    if level == '2':
+        print test_string2
+        return play_game(test_string2, parts_of_speech, answer2)
+    if level == '1':
+        print test_string1
+        return play_game(test_string1, parts_of_speech, answer1)
+    return False
 
 
 # Checks if a word in parts_of_speech is a substring of the word passed in.
@@ -65,22 +64,18 @@ def word_in_pos(word, parts_of_speech):
 def play_game(ml_string, parts_of_speech, answer):
     i = 0
     user_tries = 5
-    for word in ml_string:
+    while user_tries != 0:
         replacement = word_in_pos(ml_string, parts_of_speech)
         if replacement != None:
-            while user_tries != 0:
-                replacement = word_in_pos(ml_string, parts_of_speech)
-                if replacement != None:
-                    user_input = raw_input("Type in a value for  " + replacement + ":")
-                    if user_input == answer[i] and user_tries > 0:
-                        ml_string = ml_string.replace(replacement, answer[i])
-                        user_tries = 5
-                        i += 1
-                        print "Correct " + ml_string
-                    else:
-                        user_tries -= 1
-                        print "Incorrect, you have " + str(user_tries) + " tries left"
-                else:
-                    break
-
+            user_input = raw_input("Type in a value for  " + replacement + ":")
+            if user_input == answer[i] and user_tries > 0:
+                ml_string = ml_string.replace(replacement, answer[i])
+                user_tries = 5
+                i += 1
+                print "Correct " + ml_string
+            else:
+                user_tries -= 1
+                print "Incorrect, you have " + str(user_tries) + " tries left"
+        else:
+            break
 print gamebegin()
